1f8eff630caa006bbe5c1b17335e64b0e31434ab
nickken2532 <hoanganh.nickken@gmail.com>
2024-04-07 11:30:23 +0700

    button

--- src/components/button/GroupButton.tsx
+++ src/components/button/GroupButton.tsx
@@ -1,14 +1,1 @@
-import React, { Component } from 'react'
-import { Button } from 'antd'
-
-
-interface ButtonProps {
-  type: string;
-  size: string;
-}
-
-export default class GroupButton({type, size}: ButtonProps) {
-    return (
-        <Button
-    )
-}
+export {}
